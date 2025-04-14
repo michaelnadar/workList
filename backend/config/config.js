361042c749a70ded9ee2241f5f5683d2b09378ea
Micheal Ajit <michealajit@Micheals-MacBook-Pro.local>
2025-04-14 21:11:23 +0530

    adsa

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,11 +2,20 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD || null,
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'password',
+    database: process.env.DB_NAME || 'example_db',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    dialect: 'mysql'
   }
 };
+
