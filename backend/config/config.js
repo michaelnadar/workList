cbb85765a8f5be02315ccc033ff32c2b8254d0df
Micheal Ajit <michealajit@Micheals-MacBook-Pro.local>
2025-04-14 21:28:53 +0530

    done

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-module.exports = {
-  development: {
-    username: process.env.DB_USERNAME ,
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_NAME ,
-    host: process.env.DB_HOST ,
-    port: process.env.DB_PORT ,
-    dialect: 'mysql'
-  }
-};
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME ,
+//     password: process.env.DB_PASSWORD ,
+//     database: process.env.DB_NAME ,
+//     host: process.env.DB_HOST ,
+//     port: process.env.DB_PORT ,
+//     dialect: 'mysql'
+//   }
+// };
 
